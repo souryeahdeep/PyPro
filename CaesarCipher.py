3686ec7b94da4a6915a00b79f008715afb98a380
souryadeep <165980585+souryeahdeep@users.noreply.github.com>
2025-06-21 00:24:00 +0530

    Update CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,14 +1,34 @@
-import AcceptMessage as mx
+#Made this one learn the usage of functions and loops in Py
 
+def acceptMessage(decision):
+    message = input("Type your message: \n")
+    shift = int(input("Type your shift number: \n"))
+    caeser(message, shift, decision)
+
+
+def caeser(message, shift, decision):
+    output_text = ""
+    for i in range(0, len(message)):
+        var = message[i]
+        if not var.isalpha():
+            output_text += var
+            continue
+        j = ord(var)
+        if decision == 'decode':
+            j -= shift
+            if j < 97:
+                j += 26
+        else:
+            j += shift
+            if j > 122:
+                j -= 26
+        output_text += chr(j)
+    print(f"Here's your {decision}d message : {output_text}")
+    
 while True:
     choice = str(input("Type 'encode' to encrypt, and 'decode' to decrypt\n")).lower()
-    mx.acceptMessage(choice)
+    acceptMessage(choice)
     if input("Type yes if you want to go again. Otherwise type no\n") == 'yes':
         continue
     else:
         break
-
-# 32,40,53,76,114,172
-# 8,13,23,38,58
-# 5,10,15,20
-# 5
