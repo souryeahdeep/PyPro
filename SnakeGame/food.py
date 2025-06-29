56237576875fdcd6b48e54f730c7d7f6bc7692b9
souryadeep <165980585+souryeahdeep@users.noreply.github.com>
2025-06-29 23:08:36 +0530

    Update food.py

--- SnakeGame/food.py
+++ SnakeGame/food.py
@@ -4,6 +4,7 @@ from turtle import Turtle
 class Food(Turtle):
 
     def __init__(self):
+        """Initiate the Food"""
         super().__init__()
         self.shape("circle")
         self.penup()
@@ -12,6 +13,7 @@ class Food(Turtle):
         self.speed("fastest")
         self.refresh()
     def refresh(self):
+        """Create the Food for Snake"""
         randomX=random.randint(-280,280)
         randomY=random.randint(-280,280)
         self.goto(randomX,randomY)
