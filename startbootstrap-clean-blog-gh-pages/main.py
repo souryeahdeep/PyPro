817fdf4358ec1d9a12932b1c0eb2cb00bde3b4bc
souryadeep <165980585+souryeahdeep@users.noreply.github.com>
2025-07-18 18:25:07 +0530

    Add files via upload

---
+++ startbootstrap-clean-blog-gh-pages/main.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template
+import requests
+
+
+app=Flask(__name__)
+response=requests.get("https://api.npoint.io/ad8c79fb8795a8535d1c")
+blogs = response.json()
+
+@app.route("/")
+def home():
+    return render_template("index.html",blogs=blogs)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+    
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")   
+
+@app.route('/posts/<int:id>')
+def posts(id):
+    print(id)
+    return render_template("post.html",blog=blogs[id-1])
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
